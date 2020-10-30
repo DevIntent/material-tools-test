20274dda5638ea8593bcb0f231b5ee143b680d7f
Michael Prentice <splaktar@gmail.com>
2020-10-30 15:48:33 -0400

    feat: verify that docs example works
    - commit output as an example for users

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
 const gulp = require('gulp');
 const MaterialTools = require('angular-material-tools');
-const builder = new MaterialTools({
-  destination: 'build',
-  version: 'local',
-  themes: []
+let tools = new MaterialTools({
+  destination: './dist',
+  version: '1.2.1',
+  modules: ['menu', 'checkbox'],
+  theme: {
+    primaryPalette: 'indigo',
+    accentPalette: 'purple',
+    warnPalette: 'deep-orange',
+    backgroundPalette: 'grey'
+  }
 });
 
 gulp.task('styles', [], function (done) {
-  builder._getData().then(function (buildData) {
-    builder._buildTheme(buildData);
-    done();
-  });
+
+  const successHandler = () => console.log('Build was successful.');
+  const errorHandler = error => console.error(error);
+
+  tools.build().then(successHandler).catch(errorHandler);         // Build all JS/CSS/themes
+
+  tools.build('js').then(successHandler).catch(errorHandler);     // Only build the JS.
+  tools.build('theme').then(successHandler).catch(errorHandler);  // Only build the theme.
+  tools.build('css').then(successHandler).catch(errorHandler);    // Only build the CSS
+
+  // You can also build a subset of files.
+  tools.build('css', 'js');   // Builds both the CSS and the JS.
 });
